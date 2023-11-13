70573345959c52a363332dbfdc0c1fadf8e9b36f
aleksandar234 <49760112+aleksandar234@users.noreply.github.com>
2023-11-13 17:38:06 +0100

    Update JwtUtil.java

--- demo/src/main/java/rs/raf/demo/utils/JwtUtil.java
+++ demo/src/main/java/rs/raf/demo/utils/JwtUtil.java
@@ -9,11 +9,17 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+// Ova anotacija oznacava da je `JwtUtils` Spring komponenta, sto znaci da ce biti upravljana od strane Spring kontejnera
+// i moze se automatksi ubaciti gde god je potrebno
 @Component
 public class JwtUtil {
+    // Kada server generise JWT token,on ukljucuje potpis koji se kreira kriptografski sa tajnim kljucem.
+    // Ovaj potpis se dodaje u zaglavlje tokena. Potpis se koristi za verifikaciju da je token stvarno generisao server
+    // i da nije bio promenjen od strane neautarizovanih strana.
     private final String SECRET_KEY = "MY JWT SECRET";
 
+    // Ova metoda izvlaci sve informacije iz JWT tokena. Dekodira token, proverava njegov potpis pomocu tajnog kljuca, 
+    // i vraca sve claims sadrzane u tokenu.
     public Claims extractAllClaims(String token) {
         System.out.println("Token:" + token);
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
@@ -27,6 +33,8 @@ public class JwtUtil {
         return extractAllClaims(token).getExpiration().before(new Date());
     }
 
+    // Ova meotda generise novi JWT token za dato korisnicko ime. Postavlja claims, korisnicko ime kao podatke o predmetu (subject),
+    // vreme izdavanja i vreme isteka tokena, potpisuje ih tajnim kljucem i kompaktira token.
     public String generateToken(String username){
         Map<String, Object> claims = new HashMap<>();
         return Jwts.builder()
@@ -37,6 +45,7 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
     }
 
+    // Ova metoda validira JWT token. Proverava da li korisnicko ime u tokenu odgovara korisnickom imenu koje se nalazi u UserDetails objektu i dal je token istekao.
     public boolean validateToken(String token, UserDetails user) {
         return (user.getUsername().equals(extractUsername(token)) && !isTokenExpired(token));
     }
